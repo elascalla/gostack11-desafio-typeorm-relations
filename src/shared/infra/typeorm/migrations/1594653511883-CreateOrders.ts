a62957a3317d9d240cad8fe675b8964d1e11d843
elascalla <erick.lascalla@gmail.com>
2020-07-13 17:07:07 -0400

    Feat: Adjustings in migrations.

--- src/shared/infra/typeorm/migrations/1594653511883-CreateOrders.ts
+++ src/shared/infra/typeorm/migrations/1594653511883-CreateOrders.ts
@@ -8,7 +8,7 @@ export default class CreateOrders1594653511883 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
